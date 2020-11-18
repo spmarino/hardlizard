143c084aca2e576e8f87f1198a0200ec7394f5de
spmarino <spmarino@hotmail.com>
2020-11-18 18:15:54 -0300

    actualizacion en cartelera

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 let homePage = require('./homePage')
+let enCartelera = require('./enCartelera')
 
 let movies = homePage.leerJSON()
+let cartelera = enCartelera.leerJSON()
 
 module.exports = {
     homePage : function(req,res){
@@ -11,6 +13,11 @@ module.exports = {
         res.end()
     },
     enCartelera : function(req,res){
+        cartelera.movies.forEach(movie => {
+            res.write(` **${movie.title}** \n\n /${movie.overview}/\n\n`);
+        });
+
+        res.end()
 
     }
 }
