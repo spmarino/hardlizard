41a9beaec9eca630c94b318abab9e50f28b7d897
Cristian <cristiannicolas32@hotmail.com>
2020-11-19 20:03:59 -0300

    deje genial el home

--- src/index.js
+++ src/index.js
@@ -15,25 +15,25 @@ let preguntas = preguntasFrecuentes.leerJSON()
 
 module.exports = {
     homePage: function (req, res) {
-        res.write("Bienvenidos a DH Movies el mejor sitio para encontrar las mejores peliculas, incluso mucho mejor que Netflix, Cuevana y PopCorn")
+        res.write("   ➤ Bienvenidos a DH Movies el mejor sitio para encontrar las mejores peliculas, incluso mucho mejor que Netflix, Cuevana y PopCorn")
         res.write(`\n\n\n\n`)
-        res.write("Con un total de " + movies.length + ' peliculas')
+        res.write("   ⧐ Con un total de " + movies.length + ' peliculas')
         res.write('\n\n')
         movies.forEach(movie => {
-            res.write('    - ' + movie + '\n\n')
+            res.write('        ⦁ ' + movie + '\n\n')
         });
         res.write(`\n\n\n\n`)
-        res.write(`   Recorda que podes visitar las secciones :
+        res.write(`   ⧐ Recorda que podes visitar las secciones :
         \n
-        - En Cartelera'
+        ⦁ En Cartelera'
         \n
-        - Mas Votadas
+        ⦁ Mas Votadas
         \n
-        - Sucursales
+        ⦁ Sucursales
         \n
-        - Contacto
+        ⦁ Contacto
         \n
-        - Preguntas Frecuentes
+        ⦁ Preguntas Frecuentes
         \n\n`)
         res.end()
     },
