a5893d47ba2070e258043c020b8bc2eeb250b8b0
Cristian <cristiannicolas32@hotmail.com>
2020-11-18 21:01:41 -0300

    Todo Ordenado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-let homePage = require('./homePage')
-let enCartelera = require('./enCartelera')
+const homePage = require('./homePage')
+const enCartelera = require('./enCartelera')
 const contacto = require('./contacto')
 const masVotadas = require('./masVotadas')
 const sucursalesRequeridas = require('./sucursales')
@@ -13,14 +13,14 @@ let sucursales = sucursalesRequeridas.leerJSON()
 let preguntas = preguntasFrecuentes.leerJSON()
 
 module.exports = {
-    homePage: function(req, res) {
+    homePage: function (req, res) {
         movies.movies.forEach(movie => {
             res.write(movie.title + '\n')
         });
 
         res.end()
     },
-    enCartelera: function(req, res) {
+    enCartelera: function (req, res) {
         res.write(`/*/*En Cartelera/*/* \n\n Total de Peliculas : ${movies.movies.length}\n\n`)
         cartelera.movies.forEach(movie => {
             res.write(` **${movie.title}** \n\n /${movie.overview}/\n\n`);
@@ -28,22 +28,22 @@ module.exports = {
 
         res.end()
     },
-    contacto: function(req, res) {
+    contacto: function (req, res) {
         res.end(`Contacto ${contacto}`);
 
     },
 
-    masVotadas: function(req, res) {
+    masVotadas: function (req, res) {
 
-        let movieSevenForUp = votos.movies.filter(function(movie) {
+        let movieSevenForUp = votos.movies.filter(function (movie) {
             return movie.vote_average >= 7;
         });
 
-        let rankingPromedio = movieSevenForUp.map(function(ranking) {
+        let rankingPromedio = movieSevenForUp.map(function (ranking) {
             return ranking.vote_average;
         });
 
-        let sumaRanking = rankingPromedio.reduce(function(acum, num) {
+        let sumaRanking = rankingPromedio.reduce(function (acum, num) {
             return (acum + num);
         });
 
@@ -62,7 +62,7 @@ module.exports = {
         res.end()
 
     },
-    sucursales: function(req, res) {
+    sucursales: function (req, res) {
         res.write('Nuestras Salas');
         res.write('\n\n')
         res.write('Total de salas = 16 ')
@@ -75,7 +75,7 @@ module.exports = {
         });
         res.end()
     },
-    preguntasFrecuentes: function(req, res) {
+    preguntasFrecuentes: function (req, res) {
         res.write('Preguntas Frecuentes');
         res.write('\n\n')
         res.write('Preguntas respondidas ' + preguntas.faqs.length)
--- src/masVotadas.js
+++ src/masVotadas.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 
-
 module.exports = {
     frecuentes : './data/movies.json',
     leerJSON : function(){
